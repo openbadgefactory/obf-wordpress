cfbad95beb106e7acce3bd1aaebd63b57da01ee8
Jouni Suorsa <jouni.suorsa@discendum.com>
2015-10-15 10:52:44 +0300

    Show user displaynames in badge earners list.

--- includes/achievement-functions.php
+++ includes/achievement-functions.php
@@ -646,7 +646,7 @@ function badgeos_get_achievement_earners_list( $achievement_id = 0 ) {
 		$output .= '<h4>' . apply_filters( 'badgeos_earners_heading', __( 'People who have earned this:', 'badgeos' ) ) . '</h4>';
 		$output .= '<ul class="badgeos-achievement-earners-list achievement-' . $achievement_id . '-earners-list">';
 		foreach ( $earners as $user ) {
-			$user_content = '<li><a href="' . get_author_posts_url( $user->ID ) . '">' . get_avatar( $user->ID ) . '</a></li>';
+			$user_content = '<li class="box"><a href="' . get_author_posts_url( $user->ID ) . '">' . get_avatar( $user->ID ) . '<p>' .$user->display_name .'</p></a></li>';
 			$output .= apply_filters( 'badgeos_get_achievement_earners_list_user', $user_content, $user->ID );
 		}
 		$output .= '</ul>';
