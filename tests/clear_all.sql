29a8bf91eeda3fd85a597916b42c24babf0bd17f
Jouni Suorsa <jouni.suorsa@discendum.com>
2015-10-21 09:45:11 +0300

    Fix to previous commit.

--- tests/clear_all.sql
+++ tests/clear_all.sql
@@ -5,7 +5,7 @@ DELETE FROM wp_options WHERE option_name = 'credly_settings';
 DELETE FROM wp_posts WHERE post_type IN ('badge', 'awarding-rule', 'achievement-type');
 
 # WARNING! WILL DELETE every png from media library:
-SELECT * FROM wp_posts WHERE post_type = 'attachment' AND post_mime_type='image/png' AND post_parent IN (0, 289);
+DELETE FROM wp_posts WHERE post_type = 'attachment' AND post_mime_type='image/png' AND post_parent IN (0, 289);
 # You should also delete files from wp-content/uploads to fully clear everything to a fresh-install state
 
 # Plugin deactivate+activate restores default settings
